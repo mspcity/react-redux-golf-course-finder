efa651c78ab3c81a143f9ded08f087d4d8ab76e5
mspcity <codepark2019@gmail.com>
2020-10-14 00:08:17 -0400

    CourseCard added

--- golf-course-finder-frontend/src/components/CoursesList.js
+++ golf-course-finder-frontend/src/components/CoursesList.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
-
+import TypeFilter from './TypeFilter';
 
 export class CoursesList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      typeFilter: 'All',
+      sorted: false
+    }
+  }
+
+  renderCourses = (courses) => {
+    return courses.map(course => <CourseCard key={course.id} course={course} />)
+  }
+
   render() {
     return (
       <div>
